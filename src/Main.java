f890f7a413a801f6c4b64334f9316f512f82173d
Natalia Suwińska <s33171@pjwstk.edu.pl>
2026-03-17 09:43:07 +0100

    Book commit

---
+++ src/Main.java
@@ -0,0 +1,9 @@
+public class Main {
+
+    public static void main(String[] args){
+
+        Book one = new Book("Lord of the Rings", "Talkien", 1000, true);
+        Book two = new Book("Harry Potter", "J.K Rowling", 500, true);
+        Book three = new Book("Percy Jackson", "Rick Roidian", 360, true);
+    }
+}
