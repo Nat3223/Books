e30a72799bfb814e72808a0b61321a7cbb187cca
Nat3223 <s33171@pjwstk.edu.pl>
2026-03-17 17:43:11 +0100

    add files books

---
+++ src/Main.java
@@ -0,0 +1,37 @@
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+//public class Main {
+   // public static void main(String[] args) {
+        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
+        // to see how IntelliJ IDEA suggests fixing it.
+    //    System.out.printf("Hello and welcome!");
+
+     //   for (int i = 1; i <= 5; i++) {
+            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
+            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
+    //        System.out.println("i = " + i);
+  //      }
+ //   }
+//}
+
+public class Main {
+    public static void main(String[] args) {
+        int ifour = 4;
+        double xhalf = 0.5;
+        double four = ifour;
+        int k = 1, m, n = k + 3;
+        m=2;
+        final double two = xhalf*ifour;
+        boolean b = true;
+        if (b) System.out.println(
+                "k=" + k + " , m = " + m + ", n = " + n +
+                "\nSum by 4 is equal to" + (k+m+n)/ifour);
+        String john,
+                mary = "Mary";
+        john = "John";
+        System.out.println(john + " and " + mary);
+        john = mary;
+        System.out.println(john + "and" + mary);
+    }
+}
+
